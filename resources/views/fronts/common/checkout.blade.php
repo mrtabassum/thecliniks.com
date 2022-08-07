4382be9863b901d9131832e3296512052b3aa893
Shahbaz Tabassum <mrshahbaz.tabassum@gmail.com>
2022-08-07 21:12:09 +0500

    FINAL TEST
    FINAL TEST FOR GLC BRANCH

--- resources/views/fronts/common/checkout.blade.php
+++ resources/views/fronts/common/checkout.blade.php
@@ -1,4 +1,6 @@
-                                                                                                                                                                             
+        
+        
+        <!-- FINAL TEST -->
     <script                                                                                                                                                                                           
      src="https://code.jquery.com/jquery-1.12.4.min.js"                                                                                                                                              
      integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="                                                                                                                                 
